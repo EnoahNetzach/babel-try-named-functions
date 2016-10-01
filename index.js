720b5e083cfc53a9d75dc67d41f9bf76bb500497
Enoah Netzach <fabrizio.castellarin@prontopro.it>
2016-10-01 14:10:41 +0200

    bump plugin version

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ console.log(x())
 function§ test(e) { return `test: ${e}` }
 
 console.log(test(e = 'this')(), test('that'), test(s = 432)())
+
+function§ answer(l, u, e, a = 42) {
+  return `The Answer to ${l} ${u} and ${e} is ${a}.`
+}
+
+const p1 = answer(u='the Universe')
+const p2 = p1(l='Love')
+const p3 = p2(l='Life')
+const p4 = p3(e='What', a=-1)
+
+console.log(p4(undefined, undefined, 'Everything', 42))
